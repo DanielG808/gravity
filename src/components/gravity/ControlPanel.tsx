57fca319d3d1c34da460dc4865fa32bd66468c35
Dan Gray <daniel.w.gray1994@gmail.com>
2026-02-18 13:43:41 -0500

    modularized the plus button

--- src/components/gravity/ControlPanel.tsx
+++ src/components/gravity/ControlPanel.tsx
@@ -2,6 +2,7 @@
 
 import ControlPanelDivider from "../controls/ControlPanelDivider";
 import GravityMeter from "../controls/GravityMeter";
+import PauseButton from "../controls/PauseButton";
 import CopyrightStatement from "../layout/CopyrightStatement";
 
 type ControlPanelProps = {
@@ -198,23 +199,11 @@ export default function ControlPanel({
 
         <ControlPanelDivider />
 
-        <div className="flex flex-col gap-2">
-          <button
-            type="button"
-            onClick={onTogglePause}
-            disabled={gameOver}
-            className={[
-              "w-full rounded-lg px-3 py-2 text-sm font-medium border transition",
-              gameOver
-                ? "border-white/10 bg-white/5 text-white/40 cursor-not-allowed"
-                : paused
-                  ? "border-emerald-300/25 bg-emerald-400/10 text-emerald-100 hover:bg-emerald-400/15"
-                  : "border-amber-300/25 bg-amber-400/10 text-amber-100 hover:bg-amber-400/15",
-            ].join(" ")}
-          >
-            {paused ? "Resume" : "Pause"}
-          </button>
-        </div>
+        <PauseButton
+          paused={paused}
+          gameOver={gameOver}
+          onTogglePause={onTogglePause}
+        />
 
         <ControlPanelDivider />
 
